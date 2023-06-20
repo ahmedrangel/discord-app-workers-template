394b3f1003457d7f16d4bce1b292bde651c72e10
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-06-20 10:21:11 -0500

    Update register.js

--- src/register.js
+++ src/register.js
@@ -9,13 +9,11 @@ dotenv.config();
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 const commandsArray = Object.values(commands);
 
-(async () => {
-  try {
-    console.log("Started refreshing application (/) commands.");
-    await rest.put(Routes.applicationCommands(process.env.DISCORD_APPLICATION_ID), { body: commandsArray });
+try {
+  console.log("Started refreshing application (/) commands.");
+  await rest.put(Routes.applicationCommands(process.env.DISCORD_APPLICATION_ID), { body: commandsArray });
 
-    console.log("Successfully reloaded application (/) commands.");
-  } catch (error) {
-    console.error(error);
-  }
-})();
+  console.log("Successfully reloaded application (/) commands.");
+} catch (error) {
+  console.error(error);
+}
